406c8a1f7827d96bca9f2ccbd4b1f70b77e79058
Batuhan Kucukali <mail@batuhankucukali.com>
2020-11-20 23:24:45 +0300

    initial commit

---
+++ src/main/java/com/arch/metric/WebController.java
@@ -0,0 +1,24 @@
+package com.arch.metric;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+    Counter visitCounter;
+
+    public WebController(MeterRegistry registry) {
+        visitCounter = Counter.builder("visit_counter")
+                .description("Number of visits to the site")
+                .register(registry);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        visitCounter.increment();
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/arch/metric/MetricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arch.metric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
